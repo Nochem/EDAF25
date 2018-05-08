82cfb72b1e6df73d4a3aaa07765cf9104bc1a977
Andreas Mårdén <marden.andreas@gmail.com>
2018-05-08 09:36:51 +0200

    Create Sheet.java

---
+++ Sheet.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Sheet implements Enviroment {
+
+private TreeMap map;
+private String lastCreatedSlot;
+
+public void Sheet() {}
+
+public boolean insertSlot(String str) {}
+
+public double value(String str) {}
+
+
+}
